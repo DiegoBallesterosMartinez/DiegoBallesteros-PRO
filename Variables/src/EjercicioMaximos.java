5e5bc486a4c72112ae5512244824141edc131618
DiegoBallesterosMartinez <diegoballesterosmartinez@gmail.com>
2024-09-20 16:51:37 +0200

    Update Entrada Teclado

--- Variables/src/EjercicioMaximos.java
+++ Variables/src/EjercicioMaximos.java
@@ -19,29 +19,31 @@ public class EjercicioMaximos {
         int maximo = Integer.MAX_VALUE;
 
 
-
+        //--------------------------------------------------------------------------------------------------------------
         System.out.println("El valor minimo del byte es "+Byte.MIN_VALUE +"y el maximo es "+Byte.MAX_VALUE);
         System.out.printf("El valor minimo del byte es %d y el maximo es %d\n",Byte.MIN_VALUE, Byte.MAX_VALUE);
-
+        //--------------------------------------------------------------------------------------------------------------
         System.out.println("El valor minimo del Short es "+Byte.MIN_VALUE +"y el maximo es "+Byte.MAX_VALUE);
         System.out.printf("El valor minimo del Short es %d y el maximo es %d\n",Short.MIN_VALUE, Short.MAX_VALUE);
-
+        //--------------------------------------------------------------------------------------------------------------
         System.out.println("El valor minimo del Integer es "+Byte.MIN_VALUE +"y el maximo es "+Byte.MAX_VALUE);
         System.out.printf("El valor minimo del Integer es %d y el maximo es %d\n",Integer.MIN_VALUE, Integer.MAX_VALUE);
-
+        //--------------------------------------------------------------------------------------------------------------
         System.out.println("El valor minimo del double es "+Byte.MIN_VALUE +"y el maximo es "+Byte.MAX_VALUE);
         System.out.printf("El valor minimo del double es %.5f y el maximo es %.5f\n",Double.MIN_VALUE, Double.MAX_VALUE);
-
+        //--------------------------------------------------------------------------------------------------------------
         System.out.println("El valor minimo del long es "+Byte.MIN_VALUE +"y el maximo es "+Byte.MAX_VALUE);
         System.out.printf("El valor minimo del long es %d y el maximo es %d\n",Long.MIN_VALUE, Long.MAX_VALUE);
-
+        //--------------------------------------------------------------------------------------------------------------
         System.out.println("El valor minimo del float es "+Byte.MIN_VALUE +"y el maximo es "+Byte.MAX_VALUE);
         System.out.printf("El valor minimo del float es %2f y el maximo es %2f\n",Float.MIN_VALUE, Float.MAX_VALUE);
-
-
-
+        //--------------------------------------------------------------------------------------------------------------
 
     }
 
-
 }
+
+
+
+
+
--- Variables/src/EntradaTeclado.java
+++ Variables/src/EntradaTeclado.java
@@ -1,4 +1,31 @@
-public class EntradaTeclado
+import java.util.Scanner;
 
-{
+public class EntradaTeclado {
+
+    static Scanner lectorTeclado = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nombre; // null
+        Integer ano;
+        Integer altura;
+        Boolean experienciaTrabajo;
+
+        System.out.println("Nombre");
+        nombre = lectorTeclado.next();
+        System.out.println("Cuanto anos tienes");
+        ano = lectorTeclado.nextInt();
+        System.out.println("Cuanto mides");
+        altura = lectorTeclado.nextInt();
+        System.out.println("Tienes experiencia trabajando?");
+        experienciaTrabajo = lectorTeclado.nextBoolean();
+
+        System.out.println("Mi nombre es " +nombre +"\ntengo " +ano +"\nmido " +altura
+                +"\ntengo experiencia trabajado " +experienciaTrabajo);
+        // Mi nombre es ...(Diego) tengo ...(21) mido ...(183) y tengo experiencia trabajando ...(true)
+
+    }
+
 }
+
+
+
