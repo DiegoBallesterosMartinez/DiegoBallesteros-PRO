c39f587e84cdaadd7d792dc766dc4828c252261c
DiegoBallesterosMartinez <diegoballesterosmartinez@gmail.com>
2024-09-20 09:09:33 +0200

    update

---
+++ Estructura/src/Entrada.java
@@ -0,0 +1,38 @@
+public class Entrada {
+
+        /*
+        * comentario que sirve
+        * para poner varias
+        * líneas de explicación
+        */
+
+    public static void main (String[]args) {
+
+        // TODO me falta por declarar variables y tiparlas
+
+        // comentario de una línea
+        // String
+        System.out.print("Hola, me llamo "+"Borja"+" y tengo "+40+" años y mido "+1.74);
+        // %s   -> String
+        // %d   -> int
+        // %.2f -> .numeroDecimales float
+        System.out.printf("Hola me llamo %s y tengo %d años y mido %.2f\n","Borja",40,1.74);
+        System.out.print("Esta asignatura es "+" programación");
+        // String
+        System.out.print("Me llamo Pepe");
+        // char
+        System.out.println('a');
+        // int
+        System.out.println(2);
+        // float - double
+        System.out.println(2.345);
+        // boolean
+        System.out.println(false);
+        System.out.println(true);
+
+        // TODO Instalar plugin de corchetes
+
+    }
+
+
+}
